f0fa774b846e64981abc1158b117c3808ee8749d
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-07-07 02:16:32 +0530

    added the projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Home from "./Home";
 import Contact from "./Contact"
 import About from "./About";
+import Projects from "./Projects";
 
 
-export { Home, Contact, About }
+export { Home, Contact, About, Projects }
